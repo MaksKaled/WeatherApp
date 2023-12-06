8c46638778a5e18f75315c55f33704eef5a23789
MaksKaled <90522479+MaksKaled@users.noreply.github.com>
2023-12-06 16:14:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -43,11 +43,15 @@ getResponsesBtn.addEventListener('click',()=>{
                 break;
             case 'Clear': 
             weatherConditionVector.src = './images/clear-min.png'
+break;
+
             case 'Thunderstorm':
                 weatherConditionVector.src = './images/thunderstorm-min.png'
                 break;
             case 'Snow': 
             weatherConditionVector.src = './images/snow-min.png'
+break;
+
             case 'Rain':
                 weatherConditionVector.src = './images/rain-min.png'
                 break;
